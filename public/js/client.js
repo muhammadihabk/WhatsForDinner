1438cc860010c89f261d7747e96d6d659e185fc6
muhammadihabk <69121442+muhammadihabk@users.noreply.github.com>
2022-04-11 17:32:33 +0200

    Generation form. Cleaner logic

--- public/js/client.js
+++ public/js/client.js
@@ -1,58 +1,3 @@
-document.addEventListener('keydown', (e) => {
-    if(e.code === 'KeyE') {
-        // No ingredients
-        fetch('/meal_no_ingredients')
-        .then((res) => {
-            if(!res.ok) { throw new Error(`HTTP error. Status ${res.status}`) }
-            return res.json();
-        })
-        .then(data => {
-            let dishesDiv = document.querySelector('.dishes');
-            dishesDiv.innerHTML = '';
-            let totalPrice = 0;
-            let dishes = data.data;
-            // Get 3 unique dishes
-            let currDish = '';
-            let choosenDishes = [
-                [],
-                [],
-                [],
-            ];
-            let i = -1;
-            for(let dish of dishes) {
-                if(dish.DishName === currDish) {
-                    choosenDishes[i].push(dish.IngredientName);
-                } else {
-                    currDish = dish.DishName;
-                    i++;
-                    choosenDishes[i].push(currDish);
-                    choosenDishes[i].push(dish.IngredientName);
-                }
-                totalPrice += parseFloat(dish.Price);
-            }
-            choosenDishes.forEach(dish => {
-                let para = document.createElement('p');
-                para.innerHTML = `<b>${dish[0]}</b>: `;
-                dishesDiv.append(para);
-                for(let i = 1; i < dish.length; i++) {
-                    para.innerHTML += `${dish[i]}`;
-                    if(i < dish.length - 1) {
-                        para.innerHTML += ', ';
-                    }
-                }
-            });
-            let actionsDiv = document.querySelector('.actions');
-            actionsDiv.classList.remove('hide');
-            totalPrice = totalPrice.toFixed(2);
-            let totalPriceDiv = document.querySelector('.total-price');
-            totalPriceDiv.innerHTML = '';
-            let pricePara = document.createElement('p');
-            pricePara.innerHTML = `Total<br>${totalPrice} EGP`;
-            totalPriceDiv.append(pricePara);
-        });
-    }
-});
-
 let onFormSubmition = (e) => {
     let data = new FormData(form);
     let [arr] = data;
@@ -60,44 +5,48 @@ let onFormSubmition = (e) => {
         method: 'POST',
         body: data
     };
-    if(typeof arr !== 'undefined' && arr[0] === 'ingredients') {
-        // Ingredients
-        fetch('meal_ingredients', init)
-        .then((res) => {
-            if(!res.ok) { throw new Error(`HTTP error. Status ${res.status}`) }
-            return res.json();
-            })
-            .then((data) => {
-                let dishesDiv = document.querySelector('.dishes');
-                dishesDiv.innerHTML = '';
-                let totalPrice = 0;
-                let dishes = data.data;
-                // Get 3 unique dishes
-                let currDish = '';
-                let choosenDishes = [
-                    [],
-                    [],
-                    [],
-                ];
-                let i = -1;
-                for(let dish of dishes) {
-                    if(dish.DishName === currDish) {
-                        choosenDishes[i].push(dish.IngredientName);
-                        choosenDishes[i][1] += parseFloat(dish.Price);
-                    } else {
-                        currDish = dish.DishName;
-                        i++;
-                        choosenDishes[i].push(currDish);
-                        choosenDishes[i].push(parseFloat(dish.Price));
-                        choosenDishes[i].push(dish.IngredientName);
-                    }
-                    totalPrice += parseFloat(dish.Price);
+    fetch('/generate_meal', init)
+    .then((res) => {
+        if(!res.ok) { throw new Error(`HTTP error. Status ${res.status}`) }
+        return res.json();
+        })
+        .then((data) => {
+            let dishesDiv = document.querySelector('.dishes');
+            dishesDiv.innerHTML = '';
+            let totalPrice = 0;
+            let dishes = data.data;
+            const choosenDishes = [];
+            let currDish = '';
+            let i = 0;
+            for(let dish of dishes) {
+                if(dish.DishName !== currDish) {
+                    currDish = dish.DishName;
+                    let temp = ['', 0];
+                    temp[0] = currDish;
+                    choosenDishes[i] = temp
+                    i++;
                 }
-                choosenDishes.forEach(dish => {
+                totalPrice += parseFloat(dish.Price);
+            }
+            if(typeof arr !== 'undefined' && arr[0] === 'ingredients') {
+                currDish = '';
+                let i = 0;
+                for(let dish of dishes) {
+                    if(choosenDishes[i][0] === dish.DishName) {
+                        choosenDishes[i][1] += parseFloat(dish.Price);
+                        choosenDishes[i].push(dish.IngredientName);
+                    } else {
+                        i++;
+                    }
+                }
+            }
+            choosenDishes.forEach(dish => {
+                let dishPara = document.createElement('p');
+                dishPara.innerHTML = `<b>${dish[0]}</b>`;
+                if(typeof arr !== 'undefined' && arr[0] === 'ingredients') {
+                    dishPara.innerHTML += ': ';
                     let dishPrice = dish[1];
                     dish.splice(1, 1);
-                    let dishPara = document.createElement('p');
-                    dishPara.innerHTML = `<b>${dish[0]}</b>: `;
                     for(let i = 1; i < dish.length; i++) {
                         dishPara.innerHTML += `${dish[i]}`;
                         if(i < dish.length - 1) {
@@ -105,37 +54,8 @@ let onFormSubmition = (e) => {
                         }
                     }
                     dishPara.innerHTML += `<br><b>Price</b>: ${dishPrice.toFixed(2)}`;
-                    dishesDiv.append(dishPara);
-                });
-                let actionsDiv = document.querySelector('.actions');
-                actionsDiv.classList.remove('hide');
-                totalPrice = totalPrice.toFixed(2);
-                let totalPriceDiv = document.querySelector('.total-price');
-                totalPriceDiv.innerHTML = '';
-                let pricePara = document.createElement('p');
-                pricePara.innerHTML = `Total<br>${totalPrice} EGP`;
-                totalPriceDiv.append(pricePara);
-            })
-            .catch((error) => {
-                console.log(`error using catch: ${error.message}`);
-            });
-    } else {
-        // No ingredients
-        fetch('/meal_no_ingredients', init)
-        .then((res) => {
-            if(!res.ok) { throw new Error(`HTTP error. Status ${res.status}`) }
-            return res.json();
-        })
-        .then(data => {
-            let dishesDiv = document.querySelector('.dishes');
-            dishesDiv.innerHTML = '';
-            let totalPrice = 0;
-            let dishes = data.data;
-            dishes.forEach(dish => {
-                let para = document.createElement('p');
-                para.innerHTML = dish.DishName;
-                dishesDiv.append(para);
-                totalPrice += parseFloat(dish.Price_ingredient);
+                }
+                dishesDiv.append(dishPara);
             });
             let actionsDiv = document.querySelector('.actions');
             actionsDiv.classList.remove('hide');
@@ -145,8 +65,10 @@ let onFormSubmition = (e) => {
             let pricePara = document.createElement('p');
             pricePara.innerHTML = `Total<br>${totalPrice} EGP`;
             totalPriceDiv.append(pricePara);
+        })
+        .catch((error) => {
+            console.log(`error using catch: ${error.message}`);
         });
-    }
     e.preventDefault();
 };
 
--- server/routes/num1.js
+++ server/routes/num1.js
@@ -11,8 +11,8 @@ const num1Controller = require('../controllers/num1Controller');
 router.get('/', num1Controller.read);
 router.post('/fiind', num1Controller.find);
 // this poitns to space keydown ^| form btn
-router.post('/meal_no_ingredients', upload.none(), num1Controller.meal_no_ingredients);
-router.post('/meal_ingredients', upload.none(), num1Controller.meal_ingredients);
+// router.post('/meal_no_ingredients', upload.none(), num1Controller.meal_no_ingredients);
+router.post('/generate_meal', upload.none(), num1Controller.generateMeal);
 
 router.get('/add-dish', num1Controller.add);
 
