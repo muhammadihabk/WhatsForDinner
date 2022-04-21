7fcbd7d47ed890b98bb00e16cf7568f82a045f3f
muhammadihabk <69121442+muhammadihabk@users.noreply.github.com>
2022-04-21 15:46:49 +0200

    Setup for deployment

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
 
 app.use('/app', dishes);
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on port ${listener.address().port}`);
 });
